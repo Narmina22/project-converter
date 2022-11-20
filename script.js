29cc3eb697dceb29ddae7406074e8307ee960494
Narmina <naraibr22@gmail.com>
2022-11-20 19:49:28 +0400

    added function nulls

--- script.js
+++ script.js
@@ -5,43 +5,49 @@ const amountEl_two = document.querySelector('.amount-two');
 const rateEl1 = document.querySelector('.rate-first');
 const rateEl2 = document.querySelector('.rate-second');
 
+// function validate(inp) {
+//     inp.value = inp.value.replace(/[^\d,.]*/g, '')
+//     .replace(/([,.])[,.]+/g, '$1')
+//     .replace(/^[^\d]*(\d+([.,]\d{0,5})?).*$/g, '$1');
+// }
+
+// validate(amountEl_two)
+
+function spaces(num) {
+    num = num.split('.')
+    let newNum = '.' + num[1]
+    num = num[0]
+    const s = num.length
+    for (let i=s; i>=0; i--) {
+        for (;i<=3)
+    }
+}
+
+function nulls(num) {
+    return Number(num);
+}
+function beautify(num) {
+    num = nulls(num)
+    // num = spaces(num)
+    return num
+}
 function calculate(currencyEl_one, currencyEl_two, amountEl_one, amountEl_two) {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  console.log(currency_one, currency_two)
   fetch(`https://api.exchangerate.host/latest?base=${currency_one}`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        const rate = data.rates[currency_two];
-        rateEl1.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
-        rateEl2.innerText = `1 ${currency_two} = ${(1/rate).toFixed(4)} ${currency_one}`;
-        amountEl_two.value = (amountEl_one.value * rate).toFixed(4);
+  .then((res) => res.json())
+  .then((data) => {
+      console.log(data);
+      const rate = data.rates[currency_two];
+      rateEl1.innerText = `1 ${currency_one} = ${rate.toFixed(4)} ${currency_two}`;
+      rateEl2.innerText = `1 ${currency_two} = ${(1/rate).toFixed(4)} ${currency_one}`;
+      amountEl_two.value = beautify((amountEl_one.value * rate).toFixed(4));
     })
 }
 
-function nulls(num) {
-    if(num.split('.')[1] == '0000') num = num.split[0];
-    return num;
-}
 
-function spaces(num) {
-    const s = num.length;
-    const chars = num.split('');
-    const strWithSpaces = chars.reduceRight((acc, char, i) => {
-        const spaceOrNothing = ((((s - i) % 3) === 0) ? ' ' : '');
-        return (spaceOrNothing + char + acc);
-    }, '');
-    return ((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces);
-}
-// function onlyDigits() {
-//     this.value = this.value.replace(/[^\d\.]/g, "");
-//     if(this.value.match(/\./g).length > 1) {
-//       this.value = this.value.substr(0, this.value.lastIndexOf("."));
-//     }
-//   }
-//   document.querySelector(".amount-onne").onkeyup = onlyDigits
+
 currencyEl_one.addEventListener('change', () => {calculate(currencyEl_one, currencyEl_two, amountEl_one, amountEl_two)});
 amountEl_one.addEventListener('input', () => {calculate(currencyEl_one, currencyEl_two, amountEl_one, amountEl_two)});
 currencyEl_two.addEventListener('change', () => {calculate(currencyEl_one, currencyEl_two, amountEl_one, amountEl_two)});
